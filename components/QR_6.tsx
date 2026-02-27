b681070b83b0be4b8bdc3542544c21eeb47174bd
Jamal uddin <websitetogather@gmail.com>
2026-02-27 23:51:57 +0600

    Update QR_6.tsx

--- components/QR_6.tsx
+++ components/QR_6.tsx
@@ -116,13 +116,6 @@ const QR_6: React.FC<QRProps> = ({ activeCycle, IDBadge }) => {
         </table>
       </div>
 
-      {/* Footer Section */}
-      <div className="mt-10 flex justify-between items-start text-[11px] font-bold text-slate-800">
-        <div className="flex items-center gap-6">
-          <p>নং- --------/বাঅঅ/সে-৬/খুলনা/রিটার্ণ/ত্রৈমাসিক/আয়কর ও অন্যান্য/আদায় ও সমন্বয়/২০০৮-০৯/</p>
-          <p>তাং- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; / &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /{formatYearBN(new Date())}খ্রিঃ</p>
-        </div>
-      </div>
     </div>
   );
 };
