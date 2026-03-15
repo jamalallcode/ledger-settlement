4c4b6cb852939d678a8acffac8ea3371123723b3
Jamal uddin <websitetogather@gmail.com>
2026-03-15 14:14:32 +0600

    Update AdminLoginModal.tsx

--- components/AdminLoginModal.tsx
+++ components/AdminLoginModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 import { 
   KeyRound, X, Mail, Send, Smartphone, ShieldCheck, ShieldAlert 
 } from 'lucide-react';
