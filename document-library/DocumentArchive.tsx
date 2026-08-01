67e7e32376b39f4abf93f3a7015604ab3ecaf397
jamaluddingit <jamaluddinkh3424@gmail.com>
2026-08-01 19:42:28 +0600

    Update DocumentArchive.tsx

--- document-library/DocumentArchive.tsx
+++ document-library/DocumentArchive.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { supabase } from '../lib/supabase';
 import { ArchiveDoc } from '../types';
 import { 
