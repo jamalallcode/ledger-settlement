a8b41c55858a71c410bb717732d19162f6e8a608
jamaluddingit <jamaluddinkh3424@gmail.com>
2026-08-11 15:14:51 +0600

    ররর

--- components/CustomPeriodReceiptReport.tsx
+++ components/CustomPeriodReceiptReport.tsx
@@ -1766,8 +1766,8 @@ export const CustomPeriodReceiptReport: React.FC<CustomPeriodReceiptReportProps>
                 <div className="table-container overflow-x-auto rounded-2xl shadow-inner border border-slate-200">
                   <table id="custom-period-report-table" className="w-full text-left border-collapse table-fixed">
                     <colgroup>
-                      <col className="w-[calc(2%+2.5px)]" style={{ width: 'calc(2% + 2.5px)' }} />
-                      <col className="w-[calc(17%+15px)]" style={{ width: 'calc(17% + 15px)' }} />
+                      <col className="w-[4%]" />
+                      <col className="w-[calc(17%+20px)]" style={{ width: 'calc(17% + 20px)' }} />
                       <col className="w-[21%]" />
                       <col className="w-[calc(15%-20px)]" style={{ width: 'calc(15% - 20px)' }} />
                       <col className="w-[calc(13%+5px)]" style={{ width: 'calc(13% + 5px)' }} />
